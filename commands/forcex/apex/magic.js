092537868ed336330ab9e0c508a1b25e76dbcb15
George Preece <georgejouzuna@gmail.com>
2021-03-27 08:24:42 +0000

    refactor

--- commands/forcex/apex/magic.js
+++ commands/forcex/apex/magic.js
@@ -4,65 +4,62 @@ const fs      = require("fs");
 module.exports = class extends command.SfdxCommand {
 	static requiresUsername = true;
 	static requiresProject = true;
-	static description = "imports managed global faux classes to the Apex language server";
-	static flagsConfig = { 
-		batchsize: command.flags.integer({ char: "b", description: "retrieval batch size" })
-	};
+	static description = "imports global classes from managed packages to jorje";
 
 	async run() {
-		this.ux.startSpinner("Retrieving Apex manifest");
 		this.connection  = this.org.getConnection();
 		this.projectJson = await this.project.resolveProjectConfig();
+
+		const storageLocation = `${this.project.getPath()}/.sfdx/tools/installed-packages`
+		this.initializeDirectory(storageLocation);
 		
-		// Prepare the storage location
-		const toolsPath = `${this.project.getPath()}/.sfdx/tools/installed-packages`;
-		fs.rmdirSync(toolsPath, { recursive: true });
-		fs.mkdirSync(toolsPath, { recursive: true });
+		const recordIds = await this.fetchApexManifest();
+		await this.createFauxClasses(storageLocation, recordIds);
 
-		// Retrieve the Apex manifest
-		const recordIds = [ ];
-		for(const item of await this.connection.request("/tooling/apexManifest")) {
-			if(item.type == "CLASS" && item.namespace !== this.projectJson.namespace) {
-				recordIds.push(item.id);
-
-				const packagePath = `${toolsPath}/${item.namespace}`;
-				if(!fs.existsSync(packagePath)) {
-					fs.mkdirSync(packagePath);
-					fs.writeFileSync(
-						`${packagePath}/installed-package.json`,
-						JSON.stringify({ namespace: item.namespace }, null, "\t")
-					);
-				}
-			}
-		}
-
-		this.ux.startSpinner("Retrieving Apex managed classes");
-		this.ux.setSpinnerStatus(`0/${ recordIds.length }`);
-
-		const batchSize = this.flags.batchsize || 200;
-		for(let i = 0; i < Math.ceil(recordIds.length / batchSize); ++i) {
-			const startIndex = batchSize * i;
-			const endIndex   = startIndex + batchSize;
-
-			const qs = this.buildQueryString({
-				ids: recordIds.slice(startIndex, endIndex).join(','),
-				fields: [ "Id", "NamespacePrefix", "Name", "Body" ]
-			});
-			for(const record of await this.connection.request(`/composite/sobjects/ApexClass?${ qs }`)) {
-				// Strip the faux class of any annotations before saving
-				const body = record.Body.replace(/@\w+(?:\(.*?\))?\s*/g, '');
-				fs.writeFileSync(`${ toolsPath }/${ record.NamespacePrefix }/${ record.Name }.cls`, body);
-			}
-
-			this.ux.setSpinnerStatus(`${ Math.min(recordIds.length, endIndex) }/${ recordIds.length }`);
-		}
-		this.ux.stopSpinner(`${ recordIds.length } retrieved!`);
+		this.ux.log("You may need to restart jorje for changes to take effect");
 	}
 
-	buildQueryString(parameters) {
-		const pairs = [ ];
-		for(const key in parameters)
-			pairs.push(`${ key }=${ parameters[key] }`);
-		return pairs.join('&');
+	initializeDirectory(directory) {
+		fs.rmdirSync(directory, { recursive: true });
+		fs.mkdirSync(directory, { recursive: true });
+	}
+
+	initializePackageDirectory(directory, namespace) {
+		this.initializeDirectory(directory);
+
+		const data = JSON.stringify({ namespace }, null, "\t")
+		fs.writeFileSync(`${directory}/installed-package.json`, data);
+	}
+
+	async fetchApexManifest() {
+		this.ux.startSpinner("Retrieving Apex manifest");
+
+		const namespace = this.projectJson.namespace;
+		const result = (await this.connection.request("/tooling/apexManifest"))
+			.filter(x => x.type == "CLASS" && x.namespace !== this.projectJson)
+			.map(x => x.id);
+
+		this.ux.stopSpinner("Apex manifest retrieved successfully");
+
+		return result;
+	}
+
+	async createFauxClasses(directory, recordIds) {
+		this.ux.startSpinner(`Creating ${recordIds.length} faux classes`);
+
+		for(const record of await this.connection.retrieve("ApexClass", recordIds)) {
+			const packageDirectory = `${directory}/${record.NamespacePrefix}`;
+
+			if(!fs.existsSync(packageDirectory))
+				this.initializePackageDirectory(packageDirectory, record.NamespacePrefix);
+
+			// Strip the body of any annotations since they cause issues with 
+			// the VSCode extensions and don't provide any insights to the 
+			// language server
+			const data = record.Body.replace(/@\w+(?:\(.*?\))?\s*/g, '');
+			fs.writeFileSync(`${packageDirectory}/${record.Name}.cls`, data);
+		}
+
+		this.ux.stopSpinner("Faux classes created successfully");
 	}
 }
